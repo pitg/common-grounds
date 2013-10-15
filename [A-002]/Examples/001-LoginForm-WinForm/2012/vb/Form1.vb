0ded14c03b46a33baf9e91c0e66f7dad96dcea82
kimerran <mh.neri@gmail.com>
2013-10-15 20:28:04 +0800

    done - winforms login vb

---
+++ [A-002]/Examples/001-LoginForm-WinForm/2012/vb/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        ' Define database connection and command
+        Dim con As SqlConnection
+        Dim cmd As SqlCommand
+
+        ' Create connection object and pass connection string
+        con = New SqlConnection("Data Source=.\sqlexpress;Initial Catalog=KOIVOSNET;Integrated Security=True")
+
+        ' Open the connection
+        con.Open()
+
+        ' Create command object and pass T-SQL command together with the connection object
+        cmd = New SqlCommand("SELECT * FROM USERS WHERE Username=@Username AND Password=@Password", con)
+
+        ' Neat way of passing parameters
+        cmd.Parameters.AddWithValue("@Username", txtUsername.Text)
+        cmd.Parameters.AddWithValue("@Password", txtPassword.Text)
+
+        ' Execute the SQL command
+        Dim dr As SqlDataReader = cmd.ExecuteReader()
+
+        ' Check if a record match
+        If dr.HasRows Then
+            MsgBox("Login Successful!")
+        Else
+            MsgBox("Login failed.")
+        End If
+
+    End Sub
+
+End Class
